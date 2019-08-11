bed4dc3ad4c32e551f005c4bfb4e46f4f5a28a8e
Nickael <qaznickael@hotmail.com>
2019-08-11 16:03:15 -0300

    first commit

---
+++ src/pages/Login.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-community/async-storage'
+import { View, StyleSheet, Image, TextInput, TouchableOpacity, Text } from 'react-native';
+
+import api from '../services/api';
+
+import logo from '../assets/logo.png';
+
+export default function Login({ navigation }){
+    const [user, setUser] = useState('');
+
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user => {
+            if (user) {
+                navigation.navigate('Main', { user })
+            }
+        })
+    }, [])
+
+    async function handleLogin() {
+        const response = await api.post('/devs', { username: user })
+
+        const { _id } = response.data;
+
+        await AsyncStorage.setItem('user', _id)
+
+        navigation.navigate('Main', { user: _id });
+    }
+
+    return( 
+        <View style={styles.container}>
+            <Image source={logo} />
+
+            <TextInput 
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Digite seu usuário do Github"
+            placeholderTextColor="#999"
+            style={styles.input}
+            value={user}
+            onChangeText={setUser}
+            />
+
+            <TouchableOpacity onPress={handleLogin} style={ styles.button}>
+                <Text style={styles.buttonText}>Enviar</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#282a36',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 30
+    }, 
+
+    input: {
+        height: 46,
+        alignSelf: 'stretch',
+        backgroundColor: '#f8f8f2',
+        borderWidth: 1,
+        borderColor: '#282a36',
+        borderRadius: 4,
+        marginTop: 20,
+        paddingHorizontal: 15,
+    },
+
+    button: {
+        height: 46,
+        alignSelf: 'stretch',
+        backgroundColor: '#50fa7b',
+        borderRadius: 4,
+        marginTop: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+
+    buttonText: {
+        color: '#282a36',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+})
