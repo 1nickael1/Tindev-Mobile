fc95291aa1509cfe7df90fe2c3f38dfe63848883
Nickael <qaznickael@hotmail.com>
2019-08-17 14:31:40 -0300

    Adicionado notificações com OneSignal

--- src/index.js
+++ src/index.js
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { YellowBox } from 'react-native';
+import OneSignal from 'react-native-onesignal';
 
 YellowBox.ignoreWarnings([
-  'Unrecognized WebSocket'
+  'Unrecognized WebSocket',
+  'OneSignal'
 ])
 
 import Routes from './routes';
 
-export default function App() {
-  return (
-    <Routes />
-  );
+export default class App extends Component {
+  constructor(properties) {
+    super(properties);
+    OneSignal.init("a29e7661-57b5-460a-ad25-9a1427227657");
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+    OneSignal.configure(); 	// triggers the ids event
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived(notification) {
+    console.log("Notification received: ", notification);
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device);
+  }
+
+  render() {
+    return (
+      <Routes />
+    );
+  }
 }
